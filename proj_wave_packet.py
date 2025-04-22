6b56ce7f3372760ba80ad8e1bc1c0df2535c7b3d
Lyuma323kaze <lyumakaze@outlook.com>
2025-04-22 21:17:14 +0800

    more postprocessing introduced (return result)

--- proj_wave_packet.py
+++ proj_wave_packet.py
@@ -13,12 +13,12 @@ t_terminate = 10
 
 
 # mesh parameters
-mx = 500    # mesh point number
+mx = 256    # mesh point number
 c = 0.3 # CFL number
 a = 1 # convective wave speed
 dx = (right_x - left_x) / (mx - 1)
 dt = c * dx / a
-m = 5
+m = 20
 
 # set of mesh points and plot points
 x_range = np.arange(left_x, right_x + dx, dx)
@@ -29,6 +29,7 @@ t_plot_refined = np.linspace(0, 0.1, 10)
 # plot parameter
 # y_lim = (0, 1.2)
 y_lim = None
+m_ls = [5, 10, 15, 20]
 
 # initial condition
 def ini_condition(x, m = m):
@@ -38,9 +39,28 @@ def ini_condition(x, m = m):
     value /= m
     return value
 
+def analytical_solution(x, t, m = m):
+    value = 0
+    for i in range(m):
+        value += np.sin(2 * np.pi * (i+1) * (x - t))
+    value /= m
+    return value
+
+def compute_l2_loss(result, solution):
+    diff = solution - result
+    loss = np.linalg.norm(diff)
+    return loss
+
 item = DiffSchemes(name, dt, dx, x_range, t_range, c = c, ini_condi = ini_condition, folder = folder)
 
-item.drp(t_plot, y_lim)
-item.drp_m(t_plot, y_lim)
-item.sadrp(t_plot, y_lim)
-item.mdcd(t_plot, y_lim)
+result_drp = item.drp(t_plot, y_lim, m = m)
+result_drp_m = item.drp_m(t_plot, y_lim, m = m, Re_a = 20)
+result_sadrp = item.sadrp(t_plot, y_lim, m = m)
+result_mdcd = item.mdcd(t_plot, y_lim, m = m)
+
+ana_solution = analytical_solution(x_range, t_terminate, m = m)
+
+loss_drp = compute_l2_loss(result_drp, ana_solution)
+loss_drp_m = compute_l2_loss(result_drp_m, ana_solution)
+loss_sadrp = compute_l2_loss(result_sadrp, ana_solution)
+loss_mdcd = compute_l2_loss(result_mdcd, ana_solution)
